d3640ddae05271dfe93735306efb2b48647aee87
Alex Damaratski <alexeydomoratsky1@gmail.com>
2020-05-06 11:43:45 +0300

    xmsirius: I'am ready for 2.3 :)

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -158,12 +158,21 @@ SHRP_FLASH_MAX_BRIGHTNESS := 200
 # Check your device's recovery path, dont use blindly
 SHRP_REC := /dev/block/bootdevice/by-name/recovery
 
-# SHRP Padding Flag
-SHRP_STATUSBAR_RIGHT_PADDING := 0
-SHRP_STATUSBAR_LEFT_PADDING := 0
-
 # Recovery Type (It can be treble,normal,SAR) [Only for About Section]
-SHRP_REC_TYPE := Treble
+SHRP_REC_TYPE := Fake_AB
 
 # Recovery Type (It can be A/B or A_only) [Only for About Section]
 SHRP_DEVICE_TYPE := A_Only
+
+# SHRP Padding Flag (Only for rounded corner devices.)
+SHRP_STATUSBAR_RIGHT_PADDING := 0
+SHRP_STATUSBAR_LEFT_PADDING := 0
+
+# SHRP Express, enables on-the-fly theme patching (also persistent) + persistent lock
+SHRP_EXPRESS := true
+
+# For notch
+SHRP_NOTCH := true
+
+# For dark mode
+SHRP_DARK := true
